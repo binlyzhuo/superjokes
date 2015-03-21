d261552efc5ef1768e287c813bef5351b24dbcad
binlyzhuo <binlyzhuo@outlook.com>
2015-03-21 14:32:31 +0800

    ~commit

--- src/SuperJokes.Mobile/Controllers/HomeController.cs
+++ src/SuperJokes.Mobile/Controllers/HomeController.cs
@@ -15,9 +15,26 @@ namespace SuperJokes.Mobile.Controllers
         JokeBusinessLogic jokeLogic = new JokeBusinessLogic();
         public ActionResult Index()
         {
+          
             var items = jokeLogic.LikeMostJokesGet(20,0);
 
             return View(items);
+        }
+
+        public ActionResult JokeDetail(int jokeid)
+        {
+            var jokeinfo = jokeLogic.GetLastNextJokes(jokeid);
+            //string title = jokeinfo.Item1.Title;
+            string title = string.Format("{0}，冷笑话，成人笑话_超级冷笑话", jokeinfo.Item1.Title);
+            string description = title;
+            //SetPageSeo(title, SiteKeyWords, SiteDescription);
+            return View(jokeinfo);
+        }
+
+        public ActionResult CatrgoryList()
+        {
+            var category = jokeLogic.GetCategoryList();
+            return View(category);
         }
     }
 }
