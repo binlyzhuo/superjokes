109607405fe18b691fa102b2e0b5a72a1472ec42
binlyzhuo <binlyzhuo@outlook.com>
2015-06-04 18:33:15 +0800

    ~commit

--- src/Joke.Common/Md5.cs
+++ src/Joke.Common/Md5.cs
@@ -5,13 +5,42 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Security.Cryptography;
+
 namespace Joke.Common
 {
     public class Md5
     {
         public static string GetMd5(string source)
         {
-            return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(source, "MD5").ToLower(); 
+            return System.Web.Security.FormsAuthentication.HashPasswordForStoringInConfigFile(source, "MD5").ToLower();
+        }
+
+        public static string GetMd5String(string source)
+        {
+            MD5 md5Hash = MD5.Create();
+            string hash = GetMd5Hash(md5Hash, source);
+            return hash;
+        }
+
+        static string GetMd5Hash(MD5 md5Hash, string input)
+        {
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
         }
     }
 }
