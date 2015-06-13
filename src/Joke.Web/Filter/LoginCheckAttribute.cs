87a8f866e53a9e3184e23fabe25fca55c6ce364d
binlyzhuo <binlyzhuo@outlook.com>
2015-06-13 22:39:59 +0800

    ~commit

---
+++ src/Joke.Web/Filter/LoginCheckAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Joke.Web.Filter
+{
+    public class LoginCheckAttribute:ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+            if(filterContext.HttpContext.Request.IsAuthenticated)
+            {
+                filterContext.HttpContext.Response.Redirect("/home/index");
+            }
+        }
+    }
+}
---
+++ src/Joke.Web/Filter/WapDeviceAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Joke.Web.Filter
+{
+    public class WapDeviceAttribute : ActionFilterAttribute
+    {
+    }
+}
