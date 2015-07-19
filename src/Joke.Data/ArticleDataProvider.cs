855c755554f15392d68b6b0c93a4f8dfe6ac562f
binlyzhuo <binlyzhuo@outlook.com>
2015-07-19 17:54:21 +0800

    ~commit ArticleController.cs

---
+++ src/Joke.Data/ArticleDataProvider.cs
@@ -0,0 +1,14 @@
+﻿using Joke.Model.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joke.Data
+{
+    public class ArticleDataProvider : BaseData<T_Article>
+    {
+
+    }
+}
---
+++ src/Joke.Model/Domain/T_Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joke.Model.Domain
+{
+    /// <summary>
+    /// 文章信息
+    /// </summary>
+    [Serializable]
+    public partial class T_Article
+    {
+        public T_Article()
+        { }
+        #region Model
+        private int _id;
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int ID
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        #endregion Model
+
+    }
+}
